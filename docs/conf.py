dd23d5bb63affe984cb0b4fd3cb28e8c475574c0
VG <vivekg342@gmail.com>
2023-03-06 09:24:53 +0800

    feat: add documentation

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Dozer Python Client'
+copyright = '2023, Dozer Team'
+author = 'Dozer Team'
+release = 'MIT'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = ["common_pb2_grpc.py", "health_pb2_grpc.py"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Dozer Python Client documentation master file, created by
+   sphinx-quickstart on Mon Mar  6 09:08:10 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Dozer Python Client's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
