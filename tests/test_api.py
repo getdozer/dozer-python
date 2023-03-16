40f5ee6d5354f1a97a215c2d1218d9fc078e3ca2
Dario Pizzamiglio <mediuminvader@protonmail.com>
2023-03-16 16:08:01 +0800

    Check subprocess output

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,27 +1,27 @@
 from dozer.api import ApiClient
-from tests.helper import api_client
+from tests.helper import api_client, dozer_server
 
 
-def test_api_count(api_client: ApiClient):
+def test_api_count(dozer_server, api_client: ApiClient):
     res = api_client.count()
     assert res is not None
     assert res.count is not None
 
 
-def test_api_describe(api_client: ApiClient):
+def test_api_describe(dozer_server, api_client: ApiClient):
     res = api_client.describe()
     assert res is not None
     assert len(res) > 3
 
 
-def test_api_health(api_client: ApiClient):
+def test_api_health(dozer_server, api_client: ApiClient):
     res = api_client.health()
     assert res is not None
     # SERVING = 1
     assert res.status == 1
 
 
-def test_api_query(api_client: ApiClient):
+def test_api_query(dozer_server, api_client: ApiClient):
     res = api_client.query()
     assert res is not None
     assert res.records is not None
